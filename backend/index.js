daf38a07c5552dc380af644a25163dddacf69df5
Satish Shinde <93379962+Satishrshinde@users.noreply.github.com>
2024-01-21 22:26:29 +0530

    added fileuploader code

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require('cors');
 const mongoose = require('mongoose');
-// const xlsx = require('xlsx');
-// const fs = require('fs');
-// const multer = require('multer')
+const xlsx = require('xlsx');
+const fs = require('fs');
+const multer = require('multer')
 const User = require('./users');
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 
 const app = express();
@@ -24,7 +24,7 @@ mongoose.connect(url, {
     console.log("Error connecting to database:", error);
   });
 
-// const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "uploads/" });
 
 app.get("/", (req, resp) => {
   resp.send("its working")
@@ -98,97 +98,97 @@ app.post("/login", async (req, res) => {
   }
 });
 
-// app.post("/fileUploader", upload.single("file"), async (req, res) => {
-//   // file content with user id
-//   // then file will be stored against that user
-//   const file = req.file;
-//   const userId = req.body.userId;
-//   console.log("FILE", req.body);
-//   const workbook = xlsx.readFile(file.path);
-//   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-//   const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
+app.post("/fileUploader", upload.single("file"), async (req, res) => {
+  // file content with user id
+  // then file will be stored against that user
+  const file = req.file;
+  const userId = req.body.userId;
+  console.log("FILE", req.body);
+  const workbook = xlsx.readFile(file.path);
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
 
-//   // Assuming your Excel has a header row, skip the first row (index 0)
-//   const data = jsonData.slice(1);
+  // Assuming your Excel has a header row, skip the first row (index 0)
+  const data = jsonData.slice(1);
 
-//   // Save the file information to the user in MongoDB
-//   User.findByIdAndUpdate(
-//     userId,
-//     { $push: { files: { filename: file.originalname, path: file.path } } },
-//     { new: true }
-//   )
-//     .then((updatedUser) => {
-//       // File uploaded and user updated successfully
-//       res
-//         .status(200)
-//         .json({ message: "File uploaded successfully!", user: updatedUser });
-//     })
-//     .catch((error) => {
-//       // Error occurred while saving the data
-//       res.status(500).json({ error: "Error saving data to the database." });
-//     });
-// });
+  // Save the file information to the user in MongoDB
+  User.findByIdAndUpdate(
+    userId,
+    { $push: { files: { filename: file.originalname, path: file.path } } },
+    { new: true }
+  )
+    .then((updatedUser) => {
+      // File uploaded and user updated successfully
+      res
+        .status(200)
+        .json({ message: "File uploaded successfully!", user: updatedUser });
+    })
+    .catch((error) => {
+      // Error occurred while saving the data
+      res.status(500).json({ error: "Error saving data to the database." });
+    });
+});
 
-// function convertExcelDate(serialNumber) {
-//   const date = new Date((serialNumber - 25569) * 86400 * 1000);
-//   const year = date.getFullYear();
-//   const month = String(date.getMonth() + 1).padStart(2, "0");
-//   const day = String(date.getDate()).padStart(2, "0");
-//   const formattedDate = `${year}-${month}-${day}`;
-//   return formattedDate;
-// }
+function convertExcelDate(serialNumber) {
+  const date = new Date((serialNumber - 25569) * 86400 * 1000);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+}
 
-// app.get("/users/:userId/:fileId", (req, res) => {
-//   const { userId, fileId } = req.params;
-//   // Assuming you have a User model imported
-//   User.findOne({ _id: userId })
-//     .then((user) => {
-//       if (!user) {
-//         throw new Error("User not found");
-//       }
-//       const file = user.files.find((file) => file._id.toString() === fileId);
-//       if (!file) {
-//         console.log("error")
-//         throw new Error("File not found");
-//       }
-//       fs.readFile(file.path, (err, data) => {
-//         if (err) {
-//           console.error("Error reading file:", err);
-//           return res.status(500).send("Error reading file");
-//         }
+app.get("/users/:userId/:fileId", (req, res) => {
+  const { userId, fileId } = req.params;
+  // Assuming you have a User model imported
+  User.findOne({ _id: userId })
+    .then((user) => {
+      if (!user) {
+        throw new Error("User not found");
+      }
+      const file = user.files.find((file) => file._id.toString() === fileId);
+      if (!file) {
+        console.log("error")
+        throw new Error("File not found");
+      }
+      fs.readFile(file.path, (err, data) => {
+        if (err) {
+          console.error("Error reading file:", err);
+          return res.status(500).send("Error reading file");
+        }
 
-//         const workbook = xlsx.read(data, { type: "buffer" });
-//         const sheetName = workbook.SheetNames[0];
-//         const worksheet = workbook.Sheets[sheetName];
-//         const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
+        const workbook = xlsx.read(data, { type: "buffer" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
 
-//         // Convert Excel date values to "yyyy-mm-dd" format
-//         for (let i = 0; i < jsonData.length; i++) {
-//           const row = jsonData[i];
-//           for (let j = 0; j < row.length; j++) {
-//             const cellValue = row[j];
-//             if (typeof cellValue === "number" && cellValue > 59) {
-//               row[j] = convertExcelDate(cellValue);
-//             }
-//           }
-//         }
-//         res.json(jsonData);
-//       });
-//     })
-//     .catch((error) => {
-//       console.error("Error fetching file content:", error);
-//       res.status(404).send("File not found");
-//     });
-// });
-// const EMAIL_USER = process.env.EMAIL_USER || 'satishrshinde2014@gmail.com';
-// const EMAIL_PASS = process.env.EMAIL_PASS || 'mrdgbwxqdlylfxjp';
-// const transporter = nodemailer.createTransport({
-//   service: 'Gmail',
-//   auth: {
-//     user: EMAIL_USER,
-//     pass: EMAIL_PASS
-//   },
-// });
+        // Convert Excel date values to "yyyy-mm-dd" format
+        for (let i = 0; i < jsonData.length; i++) {
+          const row = jsonData[i];
+          for (let j = 0; j < row.length; j++) {
+            const cellValue = row[j];
+            if (typeof cellValue === "number" && cellValue > 59) {
+              row[j] = convertExcelDate(cellValue);
+            }
+          }
+        }
+        res.json(jsonData);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching file content:", error);
+      res.status(404).send("File not found");
+    });
+});
+const EMAIL_USER = process.env.EMAIL_USER || 'satishrshinde2014@gmail.com';
+const EMAIL_PASS = process.env.EMAIL_PASS || 'mrdgbwxqdlylfxjp';
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: EMAIL_USER,
+    pass: EMAIL_PASS
+  },
+});
 
 
 const sendBirthdayEmail = (recipientEmail) => {
