31ac6fb8b444aa0a263e135e8df2f5158287e0a2
Satish Shinde <93379962+Satishrshinde@users.noreply.github.com>
2024-02-27 23:39:56 +0530

    Update production.js

--- employee-react-app/src/config/production.js
+++ employee-react-app/src/config/production.js
@@ -1,4 +1,4 @@
-const DOMAIN = "https://employees-employee-react-app-ipj7.vercel.app"
+const DOMAIN = "https://employees-qvia.onrender.com/"
 export const API = {
   SIGNUP: `${DOMAIN}/signup`,
   LOGIN: `${DOMAIN}/login`,
